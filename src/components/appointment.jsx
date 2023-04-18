79fa0f7c19755b6c7644ec47496455be2a5aab2b
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 14:12:25 +0300

    calvin

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -1,13 +1,12 @@
 const Booking =() =>{
     return <div>
     <h1>Welcome to doctor blogs </h1>
-    <nav>
+    <div className="topnav">
         <a href="www.youtube.com">home</a> <br />
         <a href="www.moringaschool.com">About</a><br />
         <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a> <br />
         <a href="props.com">add appointment</a><br />
-        
-    </nav>
+    </div>
     </div>
 }
 
