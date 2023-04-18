9ad7b15eaa82c5d11b66cfa89f7efcdee80198cb
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 11:52:34 +0300

    addded the blog componet

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -1,7 +1,13 @@
 const Booking =() =>{
     return <div>
     <h1>Welcome to doctor blogs </h1>
-    
+    <nav>
+        <a href="www.youtube.com">home</a>
+        <a href="www.moringaschool.com">About</a>
+        <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
+        <a href="props.com">add appointment</a>
+        
+    </nav>
     </div>
 }
 
---
+++ src/components/blog.jsx
@@ -0,0 +1,9 @@
+const Blogs = () =>{
+    return <div>
+        <p>
+            it is iportant for one to take care of there teeth since teeth lost cannot be replaced.please brush your teeth and visit the doctor reguraly
+        </p>
+    </div>
+}
+
+export default Blogs ;
