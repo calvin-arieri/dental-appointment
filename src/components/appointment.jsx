34d031b0514dde7ce07f38fcfe81344f109ece77
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 15:00:56 +0300

    added prompt

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -5,7 +5,10 @@ const Booking =() =>{
         <a href="www.youtube.com">home</a>
         <a href="www.moringaschool.com">About</a>
         <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
-        <a href="props.com">add appointment</a>
+        <a href="props.com" onClick={function(){
+            prompt("enter your name:")
+            prompt("enter date")
+        }}>add appointment</a>
         
     </nav>
     </div>
