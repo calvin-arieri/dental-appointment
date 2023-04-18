d09e8ad18db671183ce4743a518950c4f7de85b3
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 12:47:25 +0300

    add app.css

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -2,10 +2,10 @@ const Booking =() =>{
     return <div>
     <h1>Welcome to doctor blogs </h1>
     <nav>
-        <a href="www.youtube.com">home</a>
-        <a href="www.moringaschool.com">About</a>
-        <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
-        <a href="props.com">add appointment</a>
+        <a href="www.youtube.com">home</a> <br />
+        <a href="www.moringaschool.com">About</a><br />
+        <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a> <br />
+        <a href="props.com">add appointment</a><br />
         
     </nav>
     </div>
