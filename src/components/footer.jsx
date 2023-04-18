932939c9045b4d22fef35765968e73027eadaf22
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 12:30:56 +0300

    addded footer component

---
+++ src/components/footer.jsx
@@ -0,0 +1,33 @@
+const Footer = () => {
+    return <div id="footer">
+        <div id="flex">
+            <div id="contacts">
+                Please contact us through 
+                <p>
+                    call us on: 07456459449 <br />
+
+                    <a href="www.facebook.com">INSTAGRAM</a><br />
+                    <a href="www.facebook.com">WHATSAPP</a><br />
+                    <a href="www.facebook.com">TWITTER</a><br />
+                </p>
+            </div>
+            <div id="important">
+                here are important links
+                <a href="www.youtube.com">home</a>
+                <a href="www.moringaschool.com">About</a>
+                <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
+                <a href="props.com">add appointment</a>          
+
+            </div>
+            <div id="map">
+                Our offices are located in 
+                <ol>
+                    <li>along karen road</li>
+                    <li>westlands, along parklands road</li>
+                    <li>karen, the hub</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+}
+export default Footer;
