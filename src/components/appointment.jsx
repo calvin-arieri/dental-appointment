6fa09f02b201d8e19304984848fb396dd24fbf1d
johnthairukamau <tkamau82@gmail.com>
2023-04-18 15:54:30 +0300

    john1

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -2,26 +2,14 @@ const Booking =() =>{
     return <div>
 
     <nav>
-        <a href="www.youtube.com">home</a>
+        <a href="www.youtube.com">Home</a>
         <a href="www.moringaschool.com">About</a>
-        <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
-        <a href="props.com" onClick={function(){
-            prompt("enter your name:")
-            prompt("enter date")
-        }}>add appointment</a>
+        <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">Footer</a>
+        <a href="props.com">Add appointment</a>
         
     </nav>
     </div>
 
-function EmojiButton(props) {
-    console.log(props)
-    return (
-      <button>
-        <span role="img">{props.emoji}</span>
-        {props.labelText}
-      </button>
-    )
-  }
 }
 
 export default Booking;
