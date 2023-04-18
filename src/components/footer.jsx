1b48e226c10706ef166fd4d22ff50e3856d6d8a2
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-04-18 12:35:16 +0300

    add app.css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div id="contacts">
                 Please contact us through 
                 <p>
-                    call us on: 07456459449 <br />
+                    call us on: 07456459449 <br /><br />
 
                     <a href="www.facebook.com">INSTAGRAM</a><br />
                     <a href="www.facebook.com">WHATSAPP</a><br />
@@ -13,10 +13,10 @@ const Footer = () => {
             </div>
             <div id="important">
                 here are important links
-                <a href="www.youtube.com">home</a>
+                <a href="www.youtube.com">home</a><br />
                 <a href="www.moringaschool.com">About</a>
-                <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a>
-                <a href="props.com">add appointment</a>          
+                <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer">footer</a><br />
+                <a href="props.com">add appointment</a> <br />         
 
             </div>
             <div id="map">
